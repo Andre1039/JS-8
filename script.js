da4b1152fce48c6a5682219b7198bdc4b4cf039d
andrey <andreyten2006@gmail.com>
2021-09-12 13:58:35 +0500

    JS-8

---
+++ script.js
@@ -0,0 +1,43 @@
+const square__round = document.querySelector('.square__round')
+
+// console.log(square__round);
+
+square__round.addEventListener('click', () => {
+    square__round.classList.toggle('active')
+})
+
+
+
+/////////////////////////////////////// Устаревште методы //////////////////////////////////////
+
+
+// const box = document.getElementById ('box') - через id
+// const circle = document.getElementsByClassName('circle') - через класс
+// for(let key of circle) {  }
+//     console.log(key);     } - перебор элементов
+// }                         }
+
+// const type = document.getElementsByTagName('div') - через тег
+
+
+////////////////////////////////////// Новый метод /////////////////////////////////////////////
+
+
+// const ar = document.querySelector/* 'All' */('class') }
+//                                                       }
+// ar.forEach(item => {                                  } - перебор современного метода 
+//     console.log(item);                                }
+// });                                                   }
+
+
+// ar.addEventListener('событие', () => {
+//     ar.style.backrgound = 'yellow'
+// })
+
+
+///////////////////////////////////////// События /////////////////////////////////////////////
+
+// click - нажатие
+// 
+// 
+// 
